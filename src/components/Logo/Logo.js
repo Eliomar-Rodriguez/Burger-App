04623e77407970004a30c1e4f2c2edd630023701
Eliomar Antonio Rodríguez Arguedas <earodriguez@gbm.net>
2019-07-17 08:43:32 -0700

    Implemented logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import burgerLogo from '../../assets/images/burger-logo.png';
+
+import classes from './Logo.css';
 
 const logo = props => (
-    <div>
-        
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt='MyBurger'/>
     </div>
 );
 export default logo;
