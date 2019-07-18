9c8be05f2d1ea57f03d350242cb8b1eb16efd335
Eliomar Antonio Rodríguez Arguedas <earodriguez@gbm.net>
2019-07-18 14:00:07 -0600

    Improve performance, avoid render as loop

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,13 +8,14 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     async componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
                 const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post=>{
+                const updatedPosts = posts.map(post => {
                     return {
                         ...post,
                         author: 'Eliomar'
@@ -23,9 +24,17 @@ class Blog extends Component {
                 this.setState({ posts: updatedPosts });
             })
     }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
     render() {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />
         })
         return (
             <div>
@@ -33,7 +42,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
